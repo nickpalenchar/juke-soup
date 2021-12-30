348bc00ca67c384216f8a865028bca256c8845ee
Nick Palenchar <nickpal@nickpalenchar.com>
2021-12-30 12:20:52 -0500

    home page styling, fix tag generation

--- .github/tag-and-commit.sh
+++ .github/tag-and-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LATEST_TAG="$(git tag -l 'v-*' | awk -F'-' '{print $2}' | sort -nr | head -n 1)"
-NEXT_TAG=$(( LATEST_TAG + 1 ))
+NEXT_TAG="v-$(( LATEST_TAG + 1 ))"
 
 git tag v-$NEXT_TAG
 git commit -m "Build version $NEXT_TAG"
--- src/views/SelectAQuarry.js
+++ src/views/SelectAQuarry.js
@@ -1,27 +1,54 @@
-// import '../style/input.css';
-import { useNavigate } from "react-router-dom";
-
+import {useNavigate} from "react-router-dom";
+import {CgBowl} from "react-icons/cg";
 import useUser from "../auth/identity";
 import Loading from "../components/Loading";
 import {MobilishView} from "../components/MobilishView";
+import {Button, FormControl, InputGroup} from "react-bootstrap";
 
 function SelectAQuarry(props) {
-    const navigate = useNavigate();
-    const user = useUser();
-    if (user === null) {
-      return <Loading/>
-    }
-    return (
-        <MobilishView>
-            <h1>Find a Quarry</h1>
-            <input type="text"
-                   className='input-center'
-                   placeholder='two words (e.g. "shining-pearl")'/> | <button>Enter</button>
-            <br/>
-            <hr/>
-            <h3>or <button onClick={() => navigate('/soup/new')}>create one</button></h3>
-        </MobilishView>
-    );
+  const navigate = useNavigate();
+  const user = useUser();
+  if (user === null) {
+    return <Loading/>
+  }
+  return (
+    <MobilishView>
+      <br/>
+      <br/>
+      <h1><CgBowl/> Find a Soup</h1>
+      <hr/>
+      <br/>
+      <InputGroup>
+        <InputGroup.Text>2-word phrase:</InputGroup.Text>
+        <FormControl
+          placeholder='(e.g. "shining pearl")'
+        />
+        <Button variant='outline-primary'>Find</Button>
+      </InputGroup>
+      {/*<input type="text"*/}
+      {/*       className='input-center'*/}
+      {/*       placeholder='two words (e.g. "shining-pearl")'/> | <button>Enter</button>*/}
+      <br/>
+      <div style={{
+        display: 'flex',
+        alignContent: 'center',
+        flexDirection: 'column'
+      }}>
+        <div
+          style={{
+            color: '#888',
+            textAlign: 'center',
+            padding: '16px'
+          }}>or
+        </div>
+        <Button
+          style={{width: '100%'}}
+          variant='success'
+          onClick={() => navigate('/soup/new')}>Create one</Button>
+      </div>
+
+    </MobilishView>
+  );
 }
 
 export default SelectAQuarry
