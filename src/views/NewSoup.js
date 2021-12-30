ae22ee182a7752f12e8f218f8d25a5f70227d659
Nick Palenchar <nickpal@nickpalenchar.com>
2021-12-30 13:31:09 -0500

    find a soup page mobile ready

--- src/views/NewSoup.js
+++ src/views/NewSoup.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import {Form, InputGroup, Button, FormControl} from 'react-bootstrap';
-import { FiRefreshCw } from 'react-icons/fi'
-import { useNavigate } from 'react-router-dom';
+import {useState} from 'react';
+import {Form, InputGroup, Button, FormControl, Container, Row, Col} from 'react-bootstrap';
+import {FiRefreshCw} from 'react-icons/fi'
+import {useNavigate} from 'react-router-dom';
 import Loading from "../components/Loading";
 import useUser from '../auth/identity';
 import {MobilishView} from "../components/MobilishView";
@@ -55,36 +55,68 @@ export default function NewSoup(props) {
 
   return (
     <MobilishView>
-      <h1>Open a Quarry. Discover Music.</h1>
-      <Form noValidate validated={validated} onSubmit={handleSubmit} action=''>
-        <Form.Group controlId='name'>
-          <Form.Label>Quarry Name</Form.Label>
-          <Form.Control required type="input" placeholder="(e.g. Nick's Soup)" value={quarryName} onChange={updateQuarryName}/>
-        </Form.Group>
-        <br/>
-        <InputGroup style={{width: '480px'}}>
-          <InputGroup.Text>Your 2-word phrase:</InputGroup.Text>
-          <FormControl
-            style={{textAlign: 'center', fontFamily: 'monospace', color: 'red'}}
-            readOnly
-            value={phrase[0]}
-            aria-label="Recipient's username with two button addons"
-          />
-          <FormControl
-            style={{textAlign: 'center', fontFamily: 'monospace', color: 'red'}}
-            readOnly
-            value={phrase[1]}
-            aria-label="Recipient's username with two button addons"
-          />
-          <Button variant="outline-secondary" onClick={() => setPhrase(twoWordPhrase())}><FiRefreshCw/></Button>
-        </InputGroup>
-        <Form.Text className="text-muted">This is helps friends find you.</Form.Text>
-        <br/>
-        <Button className={submitting ? 'btn-secondary' : 'btn-success'}
-                type=''
-                onClick={handleSubmit}
-                disabled={submitting}> {!submitting ? 'Create' : 'working 💦'}</Button>
-      </Form>
+      <Container fluid>
+        <Row md='auto' className='justify-content-center'>
+          <Col sm style={{padding: '0 4px 0 0'}}><h1>Start a soup,</h1></Col><Col sm style={{padding: '0'}}><h1>discover music.</h1></Col>
+        </Row>
+        <Row>
+          <hr/>
+        </Row>
+        <Row>
+          <Form noValidate validated={validated} onSubmit={handleSubmit} action=''>
+            <Form.Group controlId='name'>
+              <Form.Label>Soup Name</Form.Label>
+              <Form.Control required type="input" placeholder="(e.g. Nick's Soup)" value={quarryName}
+                            onChange={updateQuarryName}/>
+            </Form.Group>
+            <br/>
+            <div className='d-md-none' style={{textAlign: 'center'}}>Your phrase:</div>
+            <InputGroup style={{maxWidth: '480px'}}>
+              <InputGroup.Text
+                className='d-none d-md-inline'
+              >
+                Your phrase:
+              </InputGroup.Text>
+              <FormControl
+                style={{textAlign: 'center', fontFamily: 'monospace', color: 'red'}}
+                readOnly
+                value={phrase[0]}
+                aria-label="First word of phrase"
+              />
+              <FormControl
+                style={{textAlign: 'center', fontFamily: 'monospace', color: 'red'}}
+                readOnly
+                value={phrase[1]}
+                aria-label="Second word of phrase"
+              />
+              <Button
+                className='d-none d-md-inline'
+                variant="outline-secondary"
+                onClick={() => setPhrase(twoWordPhrase())}
+              >
+                <FiRefreshCw/>
+              </Button>
+            </InputGroup>
+            <Button
+              className='d-md-none'
+              style={{width: '100%'}}
+              variant="outline-secondary"
+              onClick={() => setPhrase(twoWordPhrase())}
+            >
+              <FiRefreshCw/>
+            </Button>
+
+            <Form.Text className="text-muted">This is helps friends find you.</Form.Text>
+            <br/>
+            <br/>
+            <Button className={(submitting ? 'btn-secondary' : 'btn-success')}
+                    style={{width: '100%'}}
+                    type=''
+                    onClick={handleSubmit}
+                    disabled={submitting}> {!submitting ? 'Create' : 'working 💦'}</Button>
+          </Form>
+        </Row>
+      </Container>
     </MobilishView>
   )
 }
