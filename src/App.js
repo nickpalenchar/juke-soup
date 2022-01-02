1240b5e8d0e204f3d5637ba2ab7ad8aeafb0299d
Nick Palenchar <nickpal@nickpalenchar.com>
2022-01-02 16:57:50 -0500

    wip add signInAnonymously

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import './App.css';
 import {initializeApp} from "firebase/app";
 
@@ -7,6 +7,8 @@ import NewSoup from "./views/NewSoup";
 import ConnectSpotify from "./views/ConnectSpotify";
 import QuarryFromUrl from "./views/QuarryFromUrl";
 import Soup from "./views/Soup";
+import {AuthUserContext} from "./contexts/AuthUserContext";
+import useUser from "./auth/identity";
 
 const style = {
   'display': 'flex',
@@ -15,27 +17,30 @@ const style = {
 
 function App() {
 
-    const appConfig = {
-      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-      appId: process.env.REACT_APP_FIREBASE_APP_ID
-    };
-    initializeApp(appConfig);
+  const appConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+  };
+  initializeApp(appConfig);
 
 
   return (
     <div className="App" style={style}>
-      <Routes>
-        <Route path="/" element={<SelectAQuarry/>}/>
-        <Route path="/spotifyConnect" element={<ConnectSpotify/>}/>
-        <Route path="/callback" element={<ConnectSpotify/>}/>
-        <Route path="/soup/new" element={<NewSoup/>}/>
-        <Route path="/soup/:id" element={<Soup/>}/>
-        <Route path="/:phrase" element={<QuarryFromUrl/>}/>
-      </Routes>
+      {/*TODO: can we wrap elements in a userProvider context??*/}
+      <AuthUserContext.Provider value={useUser()}>
+        <Routes>
+          <Route path="/" element={<SelectAQuarry/>}/>
+          <Route path="/spotifyConnect" element={<ConnectSpotify/>}/>
+          <Route path="/callback" element={<ConnectSpotify/>}/>
+          <Route path="/soup/new" element={<NewSoup/>}/>
+          <Route path="/soup/:id" element={<Soup/>}/>
+          <Route path="/:phrase" element={<QuarryFromUrl/>}/>
+        </Routes>
+      </AuthUserContext.Provider>
     </div>
   );
 }
--- src/auth/identity.js
+++ src/auth/identity.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { getAuth, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
 import User from '../models/User';
 
 export default function useUser() {
@@ -6,11 +7,22 @@ export default function useUser() {
 
   useEffect(() => {
     const _id = localStorage.getItem('_id');
-    User.findOrCreate({_id})
-      .then(user => {
-        localStorage.setItem('_id', user._id);
-        setUser(_id);
-      })
+    const auth = getAuth();
+    signInAnonymously(auth)
+      .then(() => true)
+      .catch(error => {
+        console.error('Could not sign in with firebase auth:\n', error.code, error.message);
+      });
+
+    onAuthStateChanged(auth, (user) => {
+      console.log('user is ', user?.uid);
+      User.findOrCreate({_id})
+        .then(user => {
+          localStorage.setItem('_id', user._id);
+          setUser(_id);
+        });
+    });
+
   });
 
   return user;
